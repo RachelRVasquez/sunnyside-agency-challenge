5bf27e617b6597847e3e0192637ab620f75c9f1f
Rachel Vasquez <rachelrvasquez@gmail.com>
2024-04-30 15:43:05 +0000

    commit all the things to set up on Github

---
+++ dist/js/sunnyside-challenge.js
@@ -0,0 +1,20 @@
+(function () {
+	const hamburger = document.querySelector('.hamburger'),
+		headerMenu = document.querySelector('.off-screen-mobile');
+
+	if ('undefined' === typeof hamburger || 'undefined' === typeof headerMenu) {
+		return;
+	}
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		headerMenu.classList.toggle('active');
+		document.body.classList.toggle('mobile-menu-active');
+
+		if (hamburger.getAttribute('aria-expanded') === 'true') {
+			hamburger.setAttribute('aria-expanded', 'false');
+		} else {
+			hamburger.setAttribute('aria-expanded', 'true');
+		}
+	});
+})();
